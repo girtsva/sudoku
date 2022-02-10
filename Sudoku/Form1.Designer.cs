6e7a6401b5aa13e147e544caa0b9a0311b372bd2
Girts Varna <93934545+girtsva@users.noreply.github.com>
2022-02-10 11:49:27 +0200

    Add files via upload

---
+++ Sudoku/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonSolution = new System.Windows.Forms.Button();
+            this.buttonVerify = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 75);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 70;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(633, 633);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            this.dataGridView1.Paint += new System.Windows.Forms.PaintEventHandler(this.dataGridView1_Paint);
+            this.dataGridView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(12, 12);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(80, 30);
+            this.buttonNewGame.TabIndex = 1;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Difficulty";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Easy",
+            "Medium",
+            "Hard"});
+            this.comboBox1.Location = new System.Drawing.Point(164, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // buttonSolution
+            // 
+            this.buttonSolution.Location = new System.Drawing.Point(521, 12);
+            this.buttonSolution.Name = "buttonSolution";
+            this.buttonSolution.Size = new System.Drawing.Size(100, 30);
+            this.buttonSolution.TabIndex = 4;
+            this.buttonSolution.Text = "Show Solution";
+            this.buttonSolution.UseVisualStyleBackColor = true;
+            this.buttonSolution.Click += new System.EventHandler(this.buttonSolution_Click);
+            // 
+            // buttonVerify
+            // 
+            this.buttonVerify.Location = new System.Drawing.Point(367, 12);
+            this.buttonVerify.Name = "buttonVerify";
+            this.buttonVerify.Size = new System.Drawing.Size(116, 30);
+            this.buttonVerify.TabIndex = 3;
+            this.buttonVerify.Text = "Verify Your Solution";
+            this.buttonVerify.UseVisualStyleBackColor = true;
+            this.buttonVerify.Click += new System.EventHandler(this.buttonVerify_Click);
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MaxInputLength = 1;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 70;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MaxInputLength = 1;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 70;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MaxInputLength = 1;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 70;
+            // 
+            // Column4
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column4.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column4.HeaderText = "Column4";
+            this.Column4.MaxInputLength = 1;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 70;
+            // 
+            // Column5
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column5.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Column5.HeaderText = "Column5";
+            this.Column5.MaxInputLength = 1;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 70;
+            // 
+            // Column6
+            // 
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column6.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Column6.HeaderText = "Column6";
+            this.Column6.MaxInputLength = 1;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 70;
+            // 
+            // Column7
+            // 
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column7.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Column7.HeaderText = "Column7";
+            this.Column7.MaxInputLength = 1;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 70;
+            // 
+            // Column8
+            // 
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column8.DefaultCellStyle = dataGridViewCellStyle8;
+            this.Column8.HeaderText = "Column8";
+            this.Column8.MaxInputLength = 1;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 70;
+            // 
+            // Column9
+            // 
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column9.DefaultCellStyle = dataGridViewCellStyle9;
+            this.Column9.HeaderText = "Column9";
+            this.Column9.MaxInputLength = 1;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 711);
+            this.Controls.Add(this.buttonVerify);
+            this.Controls.Add(this.buttonSolution);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonSolution;
+        private System.Windows.Forms.Button buttonVerify;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+    }
+}
+
---
+++ Sudoku/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // adding rows first in order to create the data grid and be able to make design changes
+            dataGridView1.Rows.Add(9);
+
+            // invoking function to color the alternating Sudoku squares
+            ColorAlternateSquares();
+
+            // setting restrictions on data grid
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            dataGridView1.AllowUserToResizeColumns = false;
+            
+            // setting difficulty level to "Easy" by default
+            comboBox1.SelectedItem = "Easy";
+
+            StartGame();
+        }
+
+        // triggering event to draw distinct 3x3 square lines
+        private void dataGridView1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), dataGridView1.Height / 3, 0, dataGridView1.Height / 3, dataGridView1.Height);
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), (int)(dataGridView1.Height / 1.5), 0, (int)(dataGridView1.Height / 1.5), dataGridView1.Height);
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), 0, dataGridView1.Height / 3, dataGridView1.Height, dataGridView1.Height / 3);
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), 0, (int)(dataGridView1.Height / 1.5), dataGridView1.Height, (int)(dataGridView1.Height / 1.5));
+        }
+
+        // function to set color of the alternating Sudoku squares to darker tone
+        private void ColorAlternateSquares()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Style.BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                }
+            }
+        }
+
+        private void StartGame()
+        {
+            ResetFontColorForAllCells();
+            ResetFontStyleForAllCells();
+            LoadValues();
+            CopyGrid();
+            HideCells();
+        }
+
+        private void ResetFontColorForAllCells()
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    cell.Style.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void ResetFontStyleForAllCells()
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    cell.Style.Font =
+                        new Font("Modern No. 20", (float)20.25, FontStyle.Bold);
+                }
+            }
+        }
+
+        // restrict input to digits 1-9 and backspace; MaxLInputLength set to 1 in Design mode for each column
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != '\b' || e.KeyChar == '0';
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            if (e.Control is DataGridViewTextBoxEditingControl tb)
+            {
+                tb.KeyPress -= dataGridView1_KeyPress;
+                tb.KeyPress += dataGridView1_KeyPress;
+            }
+        }
+
+        private void LoadValues()
+        {
+            // setting all cell values to null and readonly
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                { 
+                    cell.Value = null;
+                    cell.ReadOnly = true;
+                }
+            }
+
+            // calling recursive method to find suitable value for each cell
+            FindCellValue(0, -1);
+        }
+
+        Random random = new Random();
+
+        private bool FindCellValue(int i, int j)
+        {
+            // increase j (column) value to move in right direction to the next cell
+            // when columns end, reset column to zero
+            if (++j > 8)
+            {
+                j = 0;
+
+                // increase i (row) value to move down to the next row
+                // exit when rows end
+                if (++i > 8)
+                {
+                    return true;
+                }
+            }
+
+            var numbersLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int value;
+
+            // assign to cell a random number from numbersLeft list, check if it is not duplicate
+            // go to the next cell and check if it can be assigned with next random non-duplicate number
+            do
+            {
+                // if there is no numbers left in the list, return to the previous cell
+                // and assign a different number to it
+                if (numbersLeft.Count < 1)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = 0;
+                    return false;
+                }
+
+                // assign to cell a random number from the numbers left in the list
+                value = numbersLeft[random.Next(0, numbersLeft.Count)];
+                dataGridView1.Rows[i].Cells[j].Value = value;
+
+                // remove the assigned to cell value from the list of numbers left
+                numbersLeft.Remove(value);
+            }
+            while (!IsUniqueNumber(value, i, j) || !FindCellValue(i, j));
+
+            return true;
+        }
+
+        private bool IsUniqueNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                // check all the cells in row
+                if (i != y && Convert.ToInt32(dataGridView1.Rows[x].Cells[i].Value) == value)
+                {
+                    return false;
+                }
+                
+                // check all the cells in column
+                if (i != x && Convert.ToInt32(dataGridView1.Rows[i].Cells[y].Value) == value)
+                {
+                    return false;
+                }
+            }
+
+            // check all the cells in the given square
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) == value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        // below code necessary for hiding random cell values according to
+        // user's selected game difficulty level
+        private int _cellValuesToHideCount;
+        private void HideCells()
+        {
+            if ((string)comboBox1.SelectedItem == "Easy")
+            {
+                _cellValuesToHideCount = 20;
+            }
+
+            else if ((string)comboBox1.SelectedItem == "Medium")
+            {
+                _cellValuesToHideCount = 33;
+            }
+
+            else if ((string)comboBox1.SelectedItem == "Hard")
+            {
+                _cellValuesToHideCount = 45;
+            }
+
+            List<string> rowColCombo = new List<string>();
+
+            for (int i = 0; i < _cellValuesToHideCount; i++)
+            {
+                int randomRow;
+                int randomCol;
+                string rowCol;
+
+                do
+                {
+                    randomRow = random.Next(0, 9);
+                    randomCol = random.Next(0, 9);
+                    rowCol = randomRow.ToString() + randomCol.ToString();
+
+                } while (rowColCombo.Contains(rowCol));
+
+                rowColCombo.Add(rowCol);
+
+                // setting properties for cells with removed values
+                dataGridView1.Rows[randomRow].Cells[randomCol].Value = null;
+                dataGridView1.Rows[randomRow].Cells[randomCol].Style.ForeColor = Color.Red;
+                dataGridView1.Rows[randomRow].Cells[randomCol].ReadOnly = false;
+            }
+        }
+        
+        // code below necessary for copying datagridview to grid[][] and returning when Solution pressed
+        private readonly int[][] _grid = new int[9][];
+
+        private void InitializeGrid()
+        {
+            _grid[0] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[1] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[2] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[3] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[4] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[5] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[6] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[7] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _grid[8] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        }
+
+        private void CopyGrid()
+        {
+            InitializeGrid();
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    _grid[i][j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                }
+            }
+        }
+
+        private void RestoreGrid()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = _grid[i][j];
+                }
+            }
+        }
+
+        // check solution for empty cells and non-duplicate cells
+        private void VerifySolution()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value == null)
+                    {
+                        MessageBox.Show(@"In order to check Your solution, You have to fill in all cells first!");
+                        return;
+                    }
+                }
+            }
+
+            bool duplicateFound = false;
+
+            ResetFontStyleForAllCells();
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (!IsUniqueNumber(Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value), i, j))
+                    {
+                        //MessageBox.Show($"Duplicate found! Value {dataGridView1.Rows[i].Cells[j].Value} in cell {i}-{j}");
+                        dataGridView1.Rows[i].Cells[j].Style.Font =
+                            new Font("Modern No. 20", (float)26.25, FontStyle.Bold | FontStyle.Underline);
+                        duplicateFound = true;
+                        //return;
+                    }
+                }
+            }
+
+            if (duplicateFound)
+            {
+                MessageBox.Show(@"Duplicates found!");
+            }
+            else
+            {
+                ResetFontStyleForAllCells();
+                MessageBox.Show(@"Congratulations! Your solution seems fine!");
+            }
+        }
+
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void buttonSolution_Click(object sender, EventArgs e)
+        {
+            RestoreGrid();
+        }
+        
+        private void buttonVerify_Click(object sender, EventArgs e)
+        {
+           VerifySolution();
+        }
+    }
+}
